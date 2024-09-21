5bead899d3369955fe8d2bfaeab5d495ad1bd74c
Pohla Saromo <saromopohla@gmail.com>
2024-09-21 15:25:23 -0400

    Add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import Dashboard from './dashboard/page';
 
 export default function Home() {
   return (
-    <main className="my-10 mx-4 flex flex-col flex-grow">
-      <Dashboard />
-    </main>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
+      <main className="my-10 mx-4 flex flex-col flex-grow">
+        <Dashboard />
+      </main>
+    </>
   );
 }
